ce8c8bb449b1a9f5168375f320449a91e599d529
Gihan Sudeepa <86099252+GIHAA@users.noreply.github.com>
2023-10-24 06:51:23 +0530

    add get user input functions

--- app2.cpp
+++ app2.cpp
@@ -218,6 +218,25 @@ while(choice == "c"){
         cout<<endl;
     }
 
+
+
+    std::cout << "Enter the initial velocity of the object (m/s): ";
+    std::cin >> u;
+
+    std::cout << "Enter the angle of projection (degrees): ";
+    std::cin >> angle_degrees;
+
+    double angle_rad = angle_degrees * M_PI / 180.0;
+
+    double height = calculateHeight(u, angle_rad);
+    double time_of_flight = calculateTimeOfFlight(u, angle_rad);
+    double range = calculateRange(u, angle_rad);
+
+    std::cout << "Initial velocity: " << u << " m/s\n";
+    std::cout << "Launch angle: " << angle_degrees << " degrees\n";
+    std::cout << "Height of the projectile: " << height << " meters\n";
+    std::cout << "Time of flight: " << time_of_flight << " seconds\n";
+    std::cout << "Horizontal range: " << range << " meters\n";
     /******************splitng date and time*************************/
     
     fstream fileRead5 ("temp2.csv", ios::in);
